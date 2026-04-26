9047b3e83f10670ff01394b6d8ba2dc5fbb66f52
ViktoriiaPochtarenko <160768837+ViktoriiaPochtarenko@users.noreply.github.com>
2026-04-26 12:43:59 +0200

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+const API_KEY = '8392183-8e71f5162b0cd4e1a7a250d40';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  return axios.get(BASE_URL, { params }).then(response => response.data);
+}
